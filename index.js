706ae8cef36e16daabe5a0114bb0a4e8c0e5af92
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 00:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
-  
+
 };
 
 function destructivelyPrependKitten(name) {
-  
+
 };
 
 function destructivelyRemoveLastKitten(name) {
-  
+
 };
 
 function destructivelyRemoveFirstKitten(name) {
-  
+
 };
 
 function appendKitten(name) {
-  
+
 };
 
 function prependKitten(name) {
+
+};
+
+function removeLastKitten() {
   
 };
+
+
